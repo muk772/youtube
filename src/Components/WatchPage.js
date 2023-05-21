87ebb48c56f42f4611ba6a4c503994f799961140
Mukund Madhav <Mukund.Madhav@in.niit.com>
2023-05-22 02:19:11 +0530

    live chat started

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -4,6 +4,7 @@ import { closeMenu } from "../Utils/AppSlice";
 import { useParams } from "react-router";
 import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -14,15 +15,21 @@ const WatchPage = () => {
   }, []);
   return (
     <div className="px-5">
-      <iframe
-        width="800"
-        height="500"
-        src={"https://www.youtube.com/embed/" + params.get("v")}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+      <div className="flex flex-row">
+        <iframe
+          width="900"
+          height="500"
+          src={"https://www.youtube.com/embed/" + params.get("v")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+
+        <div className="w-96 border border-gray-600 ml-16 rounded-lg shadow-lg">
+          <LiveChat />
+        </div>
+      </div>
       <div className="">
         <CommentsContainer />
       </div>
